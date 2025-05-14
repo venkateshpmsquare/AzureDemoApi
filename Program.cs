6da915e69fb2fc1acf5e610b780b55ba2f8c60c4
FrontEnd Developer <fedeveloper1@outlook.com>
2025-05-15 01:11:22 +0530

    change to .net core 8

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 
 
 builder.Services.AddEndpointsApiExplorer();
@@ -15,10 +15,8 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
+
 
 app.UseHttpsRedirection();
 
